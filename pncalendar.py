d1b958d188bc680d4c2d92bd1edba4c6d28c92b6
Nishizawa Chiaki <nishizawa@miharu.co.jp>
2022-03-02 11:18:26 +0900

    素数計算一万回単位でスレッドを生成して計算させるようにする。但し、-mltオプション指定時

--- pncalendar.py
+++ pncalendar.py
@@ -8,7 +8,11 @@ import math
 import time as tm
 import calendar as cal
 import argparse
+import threading as thrd
+from multiprocessing import Process
 
+maxnumofcal = 10000
+outmax = 0
 mondys = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 lpmondys = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -96,7 +100,7 @@ def day2dal(day):
     if (outyear == 0) or (outyear == yr):
         if outopt == 'bnnr':
             bannerout(day, yr, mt, dday)
-        else:
+        elif outopt == 'nrml':
             normalout(day, yr, mt, dday)
         day += 1
     elif (outyear != 0) and (outyear < yr):
@@ -137,13 +141,25 @@ def do_prime(cnt, vl):
     #vl += 1
     return cnt, vl
 
+def term_prime(strv, endv):
+    cnt = 0
+    vl = strv
+    while vl <= endv:
+        cnt, vl = do_prime(cnt, vl)
+        if (0 < outmax) and (outmax <= cnt):
+            break
+
+def thrd_print(strv, endv, cnt):
+    print(f'term_prime({strv}, {endv}) {cnt}')
+
 def main():
     global outopt, primeterm, outyear
     argp = argparse.ArgumentParser(description='素数カレンダー: 0001/01/01からの日数をカウントし、それが素数となる時の年月日を表示する')
-    argp.add_argument('-of', '--outformat', choices=['nrml', 'bnnr'], default='nrml', help='表示方法選択')
+    argp.add_argument('-of', '--outformat', choices=['nrml', 'bnnr', 'none'], default='nrml', help='表示方法選択')
     argp.add_argument('-prm', '--prime', metavar='<number>', type=int, nargs='*', help='素数範囲指定:開始値 [終了値]')
     argp.add_argument('-cnt', '--count', metavar='<number>', type=int, default=0, help='表示回数指定')
     argp.add_argument('-y', '--year', metavar='<year>', type=int, default=0, help='表示対象西暦')
+    argp.add_argument('-mlt', '--multi', action='store_true', help='並列処理を有効にする')
 
     args = argp.parse_args()
 
@@ -172,10 +188,40 @@ def main():
             if (0 < outmax) and (outmax <= cnt):
                 break
     else:
-        while vl <= vlend:
-            cnt, vl = do_prime(cnt, vl)
-            if (0 < outmax) and (outmax <= cnt):
-                break
+        if not args.multi:
+            term_prime(vl, vlend)
+        else:
+            numofcal = (vlend - vl) + 1
+            if numofcal <= maxnumofcal:
+                term_prime(vl, vlend)
+            else:
+                thrdtbl = []
+                numofcalthrd = int(numofcal / maxnumofcal)
+                subend = (vl + maxnumofcal) - 1
+                ttlcnt = 0
+                for x in range(numofcalthrd):
+                    nwcnt = subend - vl + 1
+                    #thrdtbl.append(thrd.Thread(target=thrd_print, args=[vl, subend, nwcnt]))
+                    #thrdtbl.append(thrd.Thread(target=term_prime, args=[vl, subend]))
+                    thrdtbl.append(Process(target=term_prime, args=[vl, subend]))
+                    thrdtbl[-1].start()
+                    ttlcnt += nwcnt
+                    vl += maxnumofcal
+                    subend += maxnumofcal
+                fraction = int(numofcal % maxnumofcal)
+                if 0 < fraction:
+                    nwcnt = vlend - vl + 1
+                    #thrdtbl.append(thrd.Thread(target=thrd_print, args=[vl, vlend, nwcnt]))
+                    #thrdtbl.append(thrd.Thread(target=term_prime, args=[vl, vlend]))
+                    thrdtbl.append(Process(target=term_prime, args=[vl, vlend]))
+                    thrdtbl[-1].start()
+                    ttlcnt += nwcnt
+
+                for t in thrdtbl:
+                    t.join()
+
+                print(f'Total:{ttlcnt}')
+                pass
 
 if __name__ == '__main__':
     main()
